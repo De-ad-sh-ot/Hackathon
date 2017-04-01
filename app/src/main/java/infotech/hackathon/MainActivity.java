7486505595c644caaf07aecd99087ab572f5e7e6
Shubham Jindal <Shubham Jindal>
2017-04-02 03:36:31 +0530

    Bhot Kuch

--- app/src/main/java/infotech/hackathon/MainActivity.java
+++ app/src/main/java/infotech/hackathon/MainActivity.java
@@ -1,9 +1,13 @@
 package infotech.hackathon;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,28 +17,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import infotech.hackathon.maps.MapsActivity;
 
+import static infotech.hackathon.R.drawable.content_main1;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private String[] images={"https://thechroniclesofacreativemind.files.wordpress.com/2012/11/ne.jpg",
+            "http://m.sumeratravel.com/images/north-east.jpg",
+            "http://www.rainbowtravels.in/images/North%20East%203.jpg",
+            "http://indiaspend.com/wp-content/uploads/2012/06/hornbill-festival-COVER-STORY_WIDTH-414px_HT-299px.jpg"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(
-                new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -44,6 +54,15 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ImageView imageView=(ImageView) (findViewById(R.id.imageView1));
+
+        Picasso.with(this)
+                .load(images[1])
+                .fit()
+                .into(imageView);
+//
+//        imageView.setImageResource(R.drawable.north_east);
     }
 
     @Override
