71933358b9f2463c22fcc6c21f0c4e1ea82393be
yash mehrotra <yash mehrotra>
2017-04-02 17:06:15 +0530

    final

--- app/src/main/java/infotech/hackathon/Final_detail_page.java
+++ app/src/main/java/infotech/hackathon/Final_detail_page.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -76,9 +75,7 @@ public class Final_detail_page extends AppCompatActivity {
                         .into(imageView);
 
                 setTitle(pageTitle);
-
-                if(State_detail_detail.ResId_state_detail!=0)
-                    fab.hide();
+                fab.hide();
             }
 
             fab.setOnClickListener(new View.OnClickListener() {
